58cc8d4f57fa3873f851732934b07e29a2019337
PedroMartins <pedropatriota05@gmail.com>
2025-09-03 16:08:41 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,8 +1,8 @@
 # jogo da força
 # declara a palavra
 palavra = ("pistache")
-palavracen = ("_") * len(palavra)
-palavratraço = ("-" * len(palavra))
+palavracen = list("_" * len(palavra))
+palavratraço = str("-" * len(palavra))
 for i in range(len(palavra)):
     print("_", end="-")
 
@@ -10,106 +10,34 @@ for i in range(len(palavra)):
 
 print("tente adivinhar a palavra!")
 chances = int(input("quantas chances você quer ter?"))
-print("você tem ", (chances), "chances!")
+print("você tem ", (chances), "chance(s)!")
 
 # sistema do jogo
 erro = 0 # Contador de erros.
 
-# Declarando quebras.
-quebra0 = True
-quebra1 = True
-quebra2 = True
-quebra3 = True
-quebra4 = True
-quebra5 = True
-quebra6 = True
-quebra7 = True
+while erro < chances:
+    letra = input("Tente adivinhar uma letra!").lower() # Recebe a letra e transforma ela em minúscula para reconhecimento.
+    if letra in palavra:
+        print("A palavra possui essa letra!")
+        for idx, char in enumerate(palavra): # Percorre a palavra inteira procurando indíce(1,2,3 etc...) da palavra e char daquele indíce.
+            if char == letra: # Se char = letra
+                palavracen[idx] = letra # Troca a letra pela char daquela posição.
+    else:
+        print("A palavra não possui essa letra!")
+        erro += 1
 
-while erro <= (chances) or quebra0 == True: # enquanto não chega na quantidade de erros, repetir o código.
-
-    letra = str(input("Tente adivinhar uma letra!")) # obtém a letra que o usuário quer adivinhar.
-    
-# reconhecimento de letra.
-    while quebra0 == True:
-        if letra != palavra[0]:
-            print("Você escolheu uma letra incorreta!") 
+    print("".join(palavracen))
+    adivinhacao = input("Você já consegue adivinhar a palavra?").lower()
+    if adivinhacao == "sim":
+        tentativaA = input("Tente adivinhar a palavra!").lower()
+        if tentativaA == palavra:
+            print("Você acertou a palavra!")
+            print("Você ganhou o jogo!")
+            chances = 10000
+            break
         else:
-            print("Você escolheu uma letra correta!")
-            quebra0 = False
-
-        if letra != palavra[1]: 
-            print("Você escolheu uma letra incorreta!")
-        else:
-            print("Você escolheu uma letra correta!")
-            quebra0 = False
-
-        if letra != palavra[2]: 
-            print("Você escolheu uma letra incorreta!")
-        else:
-            print("Você escolheu uma letra correta!")
-            quebra0 = False
-
-        if letra != palavra[3]: 
-            print("Você escolheu uma letra incorreta!")
-        else:
-            print("Você escolheu uma letra correta!")
-            quebra0 = False
-
-        if letra != palavra[4]: 
-            print("Você escolheu uma letra incorreta!")
-        else:
-            print("Você escolheu uma letra correta!")
-            quebra0 = False
-            
-        if letra != palavra[5]: 
-            print("Você escolheu uma letra incorreta!")
-        else:
-            print("Você escolheu uma letra correta!")
-            quebra0 = False
-
-        if letra != palavra[6]: 
-            print("Você escolheu uma letra incorreta!")
-        else:
-            print("Você escolheu uma letra correta!")
-            quebra0 = False
-
-        if letra != palavra[7]: 
-            print("Você escolheu uma letra incorreta!")
-        else:
-            print("Você escolheu uma letra correta!")
-            quebra0 = False
-
-        quebra0 = False
-
-# encerra o escaneamento/reconhecimento.
-
-    # troca a letra escolhida pela letra da palavra
-
-    if letra == palavra[0]:
-        palavra.replace(letra, palavracen[0])
-
-    elif letra == palavra[1]:
-        palavra.replace(letra, palavracen[1])
-        
-    elif letra == palavra[2]:
-        palavra.replace(letra, palavracen[2])
-
-    elif letra == palavra[3]:
-        palavra.replace(letra, palavracen[3])
-
-    elif letra == palavra[4]:
-        palavra.replace(letra, palavracen[4])
-
-    elif letra == palavra[5]:
-        palavra.replace(letra, palavracen[5])
-
-    elif letra == palavra[6]:
-        palavra.replace(letra, palavracen[6])
-
-    elif letra == palavra[7]:
-        palavra.replace(letra, palavracen[7])
-
-    print(palavra, ".")
-    erro = erro + 1 # adiciona 1 no contador de erros, para quando chegar na quantidade de chances, encerrar o código.
+            print("Você errou a palavra!")
+    else:
+        print("Ok")
 
 print("acabou suas chances!")
